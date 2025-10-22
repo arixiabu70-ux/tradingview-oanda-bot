414a0ea130a504094a342e90bbc6917015ef8899
arixiabu70-ux <arixiabu70@gmail.com>
2025-10-22 22:33:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+// Healthcheck 用ルート
+app.get("/", (req, res) => {
+  res.status(200).send("Server is alive!");
+});
+
+// Webhook 受信
 app.post("/webhook", (req, res) => {
     console.log("📩 Webhook受信:", req.body);
     res.status(200).send("Webhook received!");
